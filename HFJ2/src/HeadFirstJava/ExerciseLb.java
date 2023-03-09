9f34d8c60aff69728d48d8ec280d5145855b5093
pc <andreigabriel1005@gmail.com>
2023-03-09 02:33:43 +0200

    add

--- HFJ2/src/HeadFirstJava/ExerciseLb.java
+++ HFJ2/src/HeadFirstJava/ExerciseLb.java
@@ -4,6 +4,7 @@ public class ExerciseLb {
     public static void main(String[] args) {
         int x = 1;
         while (x < 10) {
+            x = x + 1;
             if (x < 3) {
                 System.out.println("big x");
             }
