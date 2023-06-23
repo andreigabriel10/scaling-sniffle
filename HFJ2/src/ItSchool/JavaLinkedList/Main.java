79fa409758e5c71d0ae70f0e11b36adbba545894
pc <andreigabriel1005@gmail.com>
2023-06-23 13:53:05 +0300

    add

---
+++ HFJ2/src/ItSchool/JavaLinkedList/Main.java
@@ -0,0 +1,14 @@
+package ItSchool.JavaLinkedList;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Volvo");
+        cars.add("Audi");
+        cars.add("BMW");
+        cars.add("Mazda");
+        System.out.println(cars);
+    }
+}
