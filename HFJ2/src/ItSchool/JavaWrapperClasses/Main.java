70b320a72ac7a9d41ad20ce6d42d0d0f0d4ebbb5
pc <andreigabriel1005@gmail.com>
2023-06-26 02:43:00 +0300

    add

---
+++ HFJ2/src/ItSchool/JavaWrapperClasses/Main.java
@@ -0,0 +1,11 @@
+package ItSchool.JavaWrapperClasses;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer myInt = 100;
+        String myString = myInt.toString();
+        System.out.println(myString.length());
+    }
+}
